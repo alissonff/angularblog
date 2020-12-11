aa32a92312af15b4a27b34247bced895aa54d971
Alisson Franco <alisson.franco@gmail.com>
2020-12-11 16:08:52 -0500

    updates to deploy to heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 /*********************************************************************************
-* WEB422 – Assignment 05
+* WEB422 – Assignment 06
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
 * assignment has been copied manually or electronically from any other source (including web sites) or
 * distributed to other students.
 *
-* Name: Alisson Felipe Oliveira Franco Student ID: 151277183 Date: 11/25/2020
+* Name: Alisson Franco Student ID: 151277183 Date: 12/11/2020
+*
+* Online Link: _______________________________________________________________
 *
 ********************************************************************************/
 
-//LINK YOUTUBE - api/posts?page=1&perPage=10
+//LINK YOUTUBE - 
 
 import { Component } from '@angular/core';
 
